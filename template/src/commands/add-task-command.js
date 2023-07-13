9cc1a931696097e90ff448aab4bb6ddd5e545188
Ilia Popov <ilia_popov02@abv.bg>
2023-07-13 19:10:51 +0300

    Add AddTaskCommand class

--- template/src/commands/add-task-command.js
+++ template/src/commands/add-task-command.js
@@ -2,4 +2,21 @@ import { Task } from '../models/task.js';
 import { wait } from '../utils/utils.js';
 import { Command } from './command.js';
 
-export class AddTaskCommand { }
+export class AddTaskCommand extends Command {
+
+    async execute(name, status) {
+      // this line below should be copied in every single command inheritor
+      await wait(500);
+  
+      try {
+        const taskToAdd = new Task(name, status);
+        await this.taskRegistry.addTask(taskToAdd);
+  
+        // console.log('Task added successfully!');
+        return console.log('Task added successfully!');
+      } catch (e) {
+        // console.log(`Problem with adding the task: ${e.message}`)
+        return console.log(`Problem with adding the task: ${e.message}`);
+      }
+    }
+  }
